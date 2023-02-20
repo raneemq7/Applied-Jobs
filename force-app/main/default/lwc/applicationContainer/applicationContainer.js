14c790c22fa55bd862b7f03d621954d874528b67
raneemq7 <105279910+raneemq7@users.noreply.github.com>
2023-02-20 17:54:55 +0000

    Update applicationContainer.js

--- force-app/main/default/lwc/applicationContainer/applicationContainer.js
+++ force-app/main/default/lwc/applicationContainer/applicationContainer.js
@@ -1,4 +1,23 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, api } from 'lwc';
 
 
-export default class ApplicationContainer extends LightningElement {}
+export default class ApplicationContainer extends LightningElement {
+
+@track isModalOpen = false;
+
+
+openModal() {
+    // to open modal set isModalOpen tarck value as true
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    // to close modal set isModalOpen tarck value as false
+    this.isModalOpen = false;
+    this.applied = false;
+    this.finished = false;
+  }
+
+  
+
+}
